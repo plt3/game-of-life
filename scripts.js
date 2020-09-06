75bef4745198f8d2c977969ae4eefefa79d51228
Paul Taylor <pltorigami@gmail.com>
2020-09-06 15:50:34 -0400

    added startGame function to run the simulation, but simulation continues until page is reloaded

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 const WINDOW_DIM = Math.floor(window.innerHeight * .97);
 const GRID_DIV = document.getElementsByClassName('gridContainer')[0];
-const BUTTON_DIV = document.getElementsByClassName('buttonContainer')[0];
+const BUTTON_OUTER_DIV = document.getElementsByClassName('buttonContainer')[0];
+const BUTTON_DIV = document.getElementsByClassName('subButtonContainer')[0];
 const EMPTY_BACKGROUND = 'white';
 const FULL_BACKGROUND = 'black';
 
-function clearInputs() {
+function clearChildren(node) {
   // remove all child nodes of container div to make space for grid
-  while (GRID_DIV.hasChildNodes()) {
-    GRID_DIV.removeChild(GRID_DIV.firstChild);
+  while (node.hasChildNodes()) {
+    node.removeChild(node.firstChild);
   }
 }
 
 function changeBackground() {
+  // for when user clicks a grid cell to set game up
   if (event.target.style.backgroundColor === EMPTY_BACKGROUND) {
     event.target.style.backgroundColor = FULL_BACKGROUND;
   } else {
@@ -47,18 +49,79 @@ function createGrid(rowNum) {
   GRID_DIV.appendChild(table);
 }
 
+function createButtons() {
+  // set up game setup instructions and start game button
+  BUTTON_OUTER_DIV.style.height = WINDOW_DIM + 'px';
+
+  const instructionsText = document.createElement('p');
+  const startButton = document.createElement('button');
+  instructionsText.innerHTML = 'Click anywhere in the grid to place as many\
+<br>starting cells as you\'d like, then press "Start game!"';
+  startButton.innerHTML = 'Start game!';
+  startButton.id = 'start';
+  startButton.onclick = startGame;
+
+  BUTTON_DIV.appendChild(instructionsText);
+  BUTTON_DIV.appendChild(startButton);
+}
+
+function getNeighborNodes(node) {
+  const nodeId = node.id.split(',');
+  const nodeRow = parseInt(nodeId[0]);
+  const nodeCol = parseInt(nodeId[1]);
+  let liveNeighbors = 0;
+
+  for (let row = nodeRow - 1; row <= nodeRow + 1; row++) {
+    for (let col = nodeCol - 1; col <= nodeCol + 1; col++) {
+      if (`${row},${col}` !== node.id) {
+        let neighbor = document.getElementById(`${row},${col}`);
+        if (neighbor) {
+          if (neighbor.style.backgroundColor === FULL_BACKGROUND) {
+            liveNeighbors += 1;
+          }
+        }
+      }
+    }
+  }
+
+  return liveNeighbors;
+}
+
+function runGame(table) {
+  const nodesToChange = [];
+
+  for (let row of table.childNodes) {
+    for (let cell of row.childNodes) {
+      let aliveAround = getNeighborNodes(cell);
+      if (cell.style.backgroundColor === FULL_BACKGROUND && (! [2, 3].includes(aliveAround))) {
+        nodesToChange.push(cell);
+      } else if (cell.style.backgroundColor === EMPTY_BACKGROUND && aliveAround === 3) {
+        nodesToChange.push(cell);
+      }
+    }
+  }
+
+  for (let node of nodesToChange) {  // ugh can I not make this a function
+    if (node.style.backgroundColor === EMPTY_BACKGROUND) {
+      node.style.backgroundColor = FULL_BACKGROUND;
+    } else {
+      node.style.backgroundColor = EMPTY_BACKGROUND;
+    }
+  }
+}
+
 function startGame() {
-  console.log('started');
+  const instructions = BUTTON_DIV.childNodes[0];
+  const stopButton = BUTTON_DIV.childNodes[1];
+  instructions.innerHTML = 'Press "Stop game" to end the simulation';
+  stopButton.innerHTML = 'Stop game';
+  const tableElem = document.getElementsByTagName('table')[0];
+  setInterval(runGame, 250, tableElem);
 }
 
 function gameSetup() {
   const rows = parseInt(document.getElementsByTagName('input')[0].value);
-  clearInputs();
+  clearChildren(GRID_DIV);
   createGrid(rows);
-
-  const startButton = document.createElement('button');
-  startButton.innerHTML = 'Start game!';
-  startButton.onclick = startGame;
-  BUTTON_DIV.style.height = WINDOW_DIM + 'px';
-  BUTTON_DIV.appendChild(startButton);
+  createButtons();
 }
