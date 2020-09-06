732f6a55cfc088e81e54675ab6096439b7223d3e
Paul Taylor <pltorigami@gmail.com>
2020-09-05 23:09:08 -0400

    added changeBackground function to fill in grid squares when clicked and added start game button with dummy startGame function

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 const WINDOW_DIM = Math.floor(window.innerHeight * .97);
-const MAIN_DIV = document.getElementsByClassName('container')[0];
+const GRID_DIV = document.getElementsByClassName('gridContainer')[0];
+const BUTTON_DIV = document.getElementsByClassName('buttonContainer')[0];
+const EMPTY_BACKGROUND = 'white';
+const FULL_BACKGROUND = 'black';
 
 function clearInputs() {
   // remove all child nodes of container div to make space for grid
-  while (MAIN_DIV.hasChildNodes()) {
-    MAIN_DIV.removeChild(MAIN_DIV.firstChild);
+  while (GRID_DIV.hasChildNodes()) {
+    GRID_DIV.removeChild(GRID_DIV.firstChild);
   }
 }
 
-function createId(node, value) {
-  let id = document.createAttribute('id');
-  id.value = value;
-  node.setAttributeNode(id);
+function changeBackground() {
+  if (event.target.style.backgroundColor === EMPTY_BACKGROUND) {
+    event.target.style.backgroundColor = FULL_BACKGROUND;
+  } else {
+    event.target.style.backgroundColor = EMPTY_BACKGROUND;
+  }
 }
 
 function createGrid(rowNum) {
@@ -21,14 +26,17 @@ function createGrid(rowNum) {
   */
   const SQUARE_DIM = Math.floor(WINDOW_DIM / rowNum);
   const table = document.createElement('table');
+  table.style.backgroundColor = FULL_BACKGROUND;
 
   for (let i = 0; i < rowNum; i++) {
     let row = document.createElement('tr');
-    createId(row, i);
+    row.id = i;
 
     for (let j = 0; j < rowNum; j++) {
       let datum = document.createElement('td');
-      createId(datum, `${i},${j}`);
+      datum.style.backgroundColor = EMPTY_BACKGROUND;
+      datum.id = `${i},${j}`;
+      datum.onclick = changeBackground;
       row.appendChild(datum);
     }
     table.appendChild(row);
@@ -36,13 +44,21 @@ function createGrid(rowNum) {
   table.style.height = WINDOW_DIM + 'px';
   table.style.width = WINDOW_DIM + 'px';
 
-  MAIN_DIV.appendChild(table);
+  GRID_DIV.appendChild(table);
+}
+
+function startGame() {
+  console.log('started');
 }
 
 function gameSetup() {
   const rows = parseInt(document.getElementsByTagName('input')[0].value);
-
   clearInputs();
-
   createGrid(rows);
+
+  const startButton = document.createElement('button');
+  startButton.innerHTML = 'Start game!';
+  startButton.onclick = startGame;
+  BUTTON_DIV.style.height = WINDOW_DIM + 'px';
+  BUTTON_DIV.appendChild(startButton);
 }
